6c5a9ed960b2e79b3d5d1cbb5756e089785ecc24
signoli <signolisergio@gmail.com>
2021-12-19 12:00:57 -0300

    Se agregaron los views de CRUD

--- cambiar/urls.py
+++ cambiar/urls.py
@@ -2,10 +2,24 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+#from  import Post, PostView, Like, Comment
+
+from posts.views import(
+    PostDeleteView,
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('', PostListView.as_view(),name='list'),
+    path('create/', PostCreateView.as_view(),name='create'),
+    path('<slug>', PostDetailView.as_view(),name='datail'),
+    path('<slug>/update/', PostUpdateView.as_view(),name='update'),
+    path('<slug>/delete/', PostDeleteView.as_view(),name='delete')  
 ]
 
 
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,21 @@
+from django.db import models
 from django.shortcuts import render
+from django.views.generic import ListView, DeleteView, CreateView,UpdateView, DetailView
 
-# Create your views here.
+from posts.models import Post
+
+
+class PostListView(ListView):
+    model = Post
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostDeleteView(DeleteView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+
+class PostUpdateView(UpdateView):
+    model = Post
